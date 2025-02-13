b1c2983a4948b494a4459b6b21ebe0029330a47a
Riya_20 <131352850+21-ri@users.noreply.github.com>
2025-02-13 14:03:22 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,23 @@ $(document).ready(function(){
         return false;
     });
 });
+<script>
+    $(document).ready(function(){
+        $(".projects-slider").owlCarousel({
+            loop: true,
+            margin: 20,
+            nav: true,
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            responsive:{
+                0:{ items:1 },
+                600:{ items:2 },
+                1000:{ items:3 }
+            }
+        });
+    });
+</script>
 
 // inspired by 
 // https://dribbble.com/shots/920939-Portfolio-Redesign-2013-early-stage?list=searches&tag=portfolio&offset=22
